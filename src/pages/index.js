d2083a0735504c5a50191b7fcda58adb9da9e4c5
Theseregrets <rishitashaw49@gmail.com>
2021-05-11 23:46:10 +0530

    setup

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/home.scss";
+
+const Index = () => {
+  return (
+    <div>
+      <h1>This is the starting project for Web Unlocked tutorials</h1>
+    </div>
+  );
+};
+export default Index;
